2299dfebbd8752f2fa6922f5b8a474b17d5b3c75
kh4ydarov <kh4ydarovs@yandex.ru>
2024-04-03 18:09:00 +0500

    Исправлен current_time.hour

--- homework.py
+++ homework.py
@@ -8,7 +8,7 @@ def test_dark_theme_by_time():
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    if 6 <= current_time.hour <= 22:
+    if 6 <= current_time.hour < 22:
         is_dark_theme = False
     else:
         is_dark_theme = True
@@ -36,7 +36,7 @@ def test_dark_theme_by_time_and_user_choice():
     elif dark_theme_enabled_by_user is False:
         is_dark_theme = False
 
-    elif 6 <= current_time.hour <= 22 and dark_theme_enabled_by_user is None:
+    elif 6 <= current_time.hour < 22 and dark_theme_enabled_by_user is None:
         is_dark_theme = False
     else:
         is_dark_theme = True
